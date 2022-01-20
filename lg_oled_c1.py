85778cbf07662377a6574acff80c58ec2f7d081b
Nathan Li <nathan.li.1995@gmail.com>
2022-01-19 23:29:24 -0500

    added visions electronics

--- lg_oled_c1.py
+++ lg_oled_c1.py
@@ -14,27 +14,62 @@ def main():
     s = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=s)
     clean_csv(the_date)
-    # amazon(the_date, driver)
-    # canadacomputers(the_date, driver)
+    amazon(the_date, driver)
+    canadacomputers(the_date, driver)
+    visions(the_date, driver)
 
+
+def get_url(search_term, url_template, delimiter):
+    search_term = search_term.replace(" ", delimiter)
+    return url_template.format(search_term)
 
 
 def visions(the_date, driver):
-    url_template = "https://www.visions.ca/catalogue/category/ProductResults.aspx?categoryId=0&searchText=lg%20oled%20c1"
+    url_template = "https://www.visions.ca/catalogue/category/ProductResults.aspx?categoryId=0&searchText={}"
     url = get_url('lg oled c1', url_template, "%20")
     driver.get(url)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    results = soup.find('div', {'id': 'product-list'})
-    results_list = results.find_all('div', {'class': 'col-xl-3'})
+    results = soup.find('div', {'id': 'result-container-kg874'})
+    results_list = results.find_all('div', {'class': 'prodlist-itembox'})
 
     records = []
     for item in results_list:
-        record = extract_cc_record(the_date, item)
+        record = extract_visions_record(the_date, item)
         if record:
             records.append(record)
 
     write_csv(records)
+
+
+def extract_visions_record(the_date, item):
+    description = item.find('div', {'class': 'prodlist-title'}).a.text
+    if "c1" not in description.lower():
+        return None
+    regex = re.search(r"\s(\d\d)\D\s", description)
+    if regex is None:
+        inches = ''
+    else:
+        inches = regex.group().strip().replace('"', "").replace("”", "")
+    try:
+        rating = item.find('div', {'class': 'pr-snippet-rating-decimal'}).text
+    except AttributeError:
+        rating = 0
+    try:
+        review_count_string = item.find('div', {'class': 'pr-category-snippet__total pr-category-snippet__item'}).text
+        if review_count_string != "No Reviews":
+            review_count = [int(s) for s in review_count_string.split() if s.isdigit()][0]
+        else:
+            review_count = 0
+    except AttributeError:
+        review_count = 0
+    string_price = item.find('span', {'class': 'saleprice-432gh'}).text
+    price = ""
+    for s in string_price:
+        if s.isdigit() or s == ".":
+            price += s
+
+    return the_date, description, inches, float(price), float(rating), int(review_count), "visions.ca"
 
 
 def canadacomputers(the_date, driver):
@@ -73,7 +108,7 @@ def extract_cc_record(the_date, item):
 
 def amazon(the_date, driver):
     url_template = "https://www.amazon.ca/s?k={}&ref=nb_sb_noss"
-    url = get_url('lg oled c1 series', url_template)
+    url = get_url('lg oled c1 series', url_template, "+")
     driver.get(url)
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -86,11 +121,6 @@ def amazon(the_date, driver):
             records.append(record)
 
     write_csv(records)
-
-
-def get_url(search_term, url_template, delimiter):
-    search_term = search_term.replace(" ", delimiter)
-    return url_template.format(search_term)
 
 
 def extract_amazon_record(the_date, item):
@@ -111,15 +141,20 @@ def extract_amazon_record(the_date, item):
     except AttributeError:
         return None
     try:
-        rating = item.i.text
+        rating_text = item.i.text
+        regex_rating = re.search(r"(.{1,3}) out of .{1,3} stars", rating_text)
+        if regex_rating is None:
+            rating = 0
+        else:
+            rating = regex_rating.group(1).strip()
     except AttributeError:
-        rating = ''
+        rating = 0
     try:
         review_count = item.find('span', {'class': 'a-size-base'}).text
     except AttributeError:
         review_count = 0
 
-    return the_date, description, inches, float(price), rating, review_count, "amazon.ca"
+    return the_date, description, inches, float(price), float(rating), int(review_count), "amazon.ca"
 
 
 def clean_csv(the_date):
