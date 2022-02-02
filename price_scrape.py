6721f30b1a66326e5e72c2bf31f18f60b7d9923b
Nathan Li <nathan.li.1995@gmail.com>
2022-02-02 00:14:02 -0500

    optimize imports

--- price_scrape.py
+++ price_scrape.py
@@ -8,10 +8,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 
 def main():
